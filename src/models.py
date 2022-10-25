4f7eae6991db5278d3d93e96804738b0bbebf700
Lconforme <Lconforme06@yahoo.com>
2022-10-25 00:46:47 +0000

    starwars diagram

--- src/models.py
+++ src/models.py
@@ -10,10 +10,50 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=True)
+    gender = Column(String(250), nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    height = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=True)
+    home_world_id = Column(Integer, ForeignKey('planet.id'))
+    home_world = relationship('Planet')
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    diameter = Column(String(250))
+    rotational_period = Column(String(250))
+    orbital_period = Column(String(250))
+    gravity = Column(String(250))
+    population = Column(String(250))
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(String(250))
+
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    model = Column(String(250))
+    starship_class = Column(String(250))
+    manufactuer = Column(String(250))
+    cost_in_credits = Column(String(250))
+    length = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250))
+    max_atmosphering_speed = Column(String(250))
+    hyperdrive_rating = Column(String(250))
+    MGLT = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
+
 
 class Address(Base):
     __tablename__ = 'address'
